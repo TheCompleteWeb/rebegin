9b3728b7ff22f75e989910abf720c56c6fa9f38a
neogeoss <neogeo-s@hanmail.net>
2018-03-01 23:53:08 +0900

    stalker answer

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ var userFName = prompt("What is your first name?");
 var userLName = prompt("What is your last name?");
 var age = prompt("What is your age?");
 alert("Nice to meet you ," + userFName + " " + userLName);
-console.log("You are  " + userName + " years old.");
+console.log("You are  " + age + " years old.");
 
