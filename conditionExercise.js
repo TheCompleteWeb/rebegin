47ce74181bea5dffdeeb289be636e64e73c93cd6
neogeoss <neogeo-s@hanmail.net>
2018-03-07 00:19:38 +0900

    new Exercise

---
+++ conditionExercise.js
@@ -0,0 +1,11 @@
+var age = 18;
+
+if(age<0){
+    console.log("Age cannot be an negative no.")
+} else if( age == 21){
+    console.log("Happy 21st Birthday!");
+} else if( age % 2 != 0){
+    console.log("Your age is odd");
+} else if( Math.sqrt(age) == 0){
+    console.log("Perfect square!");
+}
